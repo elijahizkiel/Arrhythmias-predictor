d6a85915473c144669d512dda0dd027bd7f42041
=elijahizkiel <=>
2024-12-18 16:27:28 +0300

    add jsx file to manage images assets

---
+++ front-end/predictor-ui/src/assets/imgs.js
@@ -0,0 +1,9 @@
+import doctorSignup from './doctor-signup-page.jpg';  
+import doctorLanding from './doctor-signup-page.jpg';
+
+const assets = {
+    doctorSignup,
+    doctorLanding,
+}
+
+export default assets
