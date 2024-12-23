64be6003e0fb30aa580e9394c4ba7c2dc0c72288
=elijahizkiel <=>
2024-12-24 00:48:29 +0300

    validate input before sending to server

--- front-end/predictor-ui/src/components/LoginSignupForm.jsx
+++ front-end/predictor-ui/src/components/LoginSignupForm.jsx
@@ -1,17 +1,18 @@
 import InputField from "./InputField";
 import Button from "./Button";
 import Label from "./Label";
+import { useState } from "react";
 
 function LoginSignupForm({
-  hasAccount,
-  onSignup,
-  onCreatAccount,
+  onCreateAccount,
   onContinueWithGoogle,
   onLogin,
+  onHome,
 }) {
+  let [hasAccount, setHasAccount] = useState(false);
   return (
     <div className="login-form">
-      <Label classes='login-logo' text="we care" />
+      <Label classes="login logo" text="we care" onClick={onHome} />
       <h1>{hasAccount ? "Login" : "Create Account"}</h1>
       {!hasAccount && (
         <div className="name-fields">
@@ -41,7 +42,7 @@ function LoginSignupForm({
         <InputField
           type="text"
           classes="username-field input"
-          id="username"
+          id="user-name"
           label="User Name"
           required={true}
         />
@@ -57,14 +58,45 @@ function LoginSignupForm({
         text={hasAccount ? "Login" : "Create Account"}
         classes="btn primary"
         style={{ margin: "2vh 1vh" }}
-        sendRequest={hasAccount ? onLogin : onCreatAccount}
+        sendRequest={
+          hasAccount
+            ? () => {
+                const [email, password] = [
+                  document.getElementById("first-name").value,
+                  document.getElementById("last-name").value,
+                  document.getElementById("user-name").value,
+                  document.getElementById("user-email").value,
+                  document.getElementById("user-password").value,
+                ];
+                if( email===''|| password===''){
+                  window.alert('please insert all indicated necessary fileds')
+                  return;}
+                onLogin(email, password);
+              }
+            : () => {
+                const [firstName, lastName, userName, email, password] = [
+                  document.getElementById("first-name").value,
+                  document.getElementById("last-name").value,
+                  document.getElementById("user-name").value,
+                  document.getElementById("user-email").value,
+                  document.getElementById("user-password").value,
+                ];
+                if(firstName ===''|| userName===''|| email===''|| password===''){
+                  window.alert('please insert all indicated necessary fileds')
+                  return;
+                }
+                onCreateAccount(firstName, lastName, userName, email, password);
+              }
+        }
       />
       <Label
         text={hasAccount ? "Don't have account? " : "Alreadry have account? "}
       >
         <span
           style={{ color: " #0b9cbd", cursor: "pointer" }}
-          onClick={hasAccount ? onSignup : onLogin}
+          onClick={() => {
+            setHasAccount(!hasAccount);
+          }}
         >
           {hasAccount ? "Signup" : "Login"}
         </span>
