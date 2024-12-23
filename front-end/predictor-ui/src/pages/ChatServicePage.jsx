b4154f590655bf7e249ba4a8ce5f760babb5646e
=elijahizkiel <=>
2024-12-24 00:17:05 +0300

    add api to insert avatar of the initial of the user

--- front-end/predictor-ui/src/pages/ChatServicePage.jsx
+++ front-end/predictor-ui/src/pages/ChatServicePage.jsx
@@ -3,18 +3,21 @@ import MessagingField from "../components/MessagingField";
 import UserAvatar from "../components/UserAvatar";
 import Label from "../components/Label";
 
-function ChatServicePage({ chats, user }) {
+function ChatServicePage({ chats, onHome, user }) {
   return (
     <>
       <header className="chat-header">
-        <Label classes="logo" text="we care" />
-        <UserAvatar imgSource={''} />
+        <Label classes="logo" text="we care" onClick={onHome} />
+        <UserAvatar
+          imgSource={`https://avatar.iran.liara.run/username?username=${user.firstName}+${user.lastName}`}
+        />
       </header>
       <main>
-        <MessagesArea messages={(chats !== null && chats !== undefined )?chats: null} />
+        <MessagesArea
+          messages={chats !== null && chats !== undefined ? chats : null}
+        />
         <MessagingField classes={"message-input"} />
       </main>
-
     </>
   );
 }
