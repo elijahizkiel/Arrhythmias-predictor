4fd3cdda72dd01fb9bce07cb80d2358a27d10c2e
=elijahizkiel <=>
2025-01-16 10:16:00 +0300

    add pointer hand image to be used in diagnose page

--- front-end-app/src/assets/imgs.js
+++ front-end-app/src/assets/imgs.js
@@ -1,10 +1,12 @@
 import doctorSignup from './doctor-signup-page.png';  
 import doctorLanding from './doctor-landing-page.png';
 import listBullet from './list-bullet.png'
+import pointerHand from './pointer_hand.png'
 const assets = {
     doctorSignup,
     doctorLanding,
     listBullet,
+    pointerHand,
 }
 
 export default assets
