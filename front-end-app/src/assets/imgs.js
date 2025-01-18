8a5baf13ee2fbb094ee0243625e6e432955ca31c
=elijahizkiel <=>
2025-01-18 17:45:08 +0300

    import stetescope img for landing page ui

--- front-end-app/src/assets/imgs.js
+++ front-end-app/src/assets/imgs.js
@@ -2,11 +2,13 @@ import doctorSignup from './doctor-signup-page.png';
 import doctorLanding from './doctor-landing-page.png';
 import listBullet from './list-bullet.png'
 import pointerHand from './pointer_hand.png'
+import stetescope from './stethescope.png'
 const assets = {
     doctorSignup,
     doctorLanding,
     listBullet,
     pointerHand,
+    stetescope,
 }
 
 export default assets
