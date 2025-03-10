79e93be0f228bf79d4875704af1f8e23db72ea37
=elijahizkiel <=>
2025-03-10 19:53:55 +0300

    refactor LandingPage component to use React Router for navigation and improve contact list handling

--- front-end-app/src/pages/LandingPage.jsx
+++ front-end-app/src/pages/LandingPage.jsx
@@ -2,101 +2,113 @@ import Button from "../components/Button.jsx";
 import Label from "../components/Label.jsx";
 import Card from "../components/Card.jsx";
 import assets from "../assets/imgs.js";
+import { useNavigate } from "react-router-dom";
+import { useRef } from "react";
 
-
-function LandingPage({ onDiagnose, onChatService }) {
+function LandingPage() {
   let contacts = [
-      { name: "LandMark Hospital", phone: "+251 115 525 463" },
-      { name: "Addis Cardiac center", phone: "+251 116 634 740" },
-      { name: "Addis Hiwot General Hospital", phone: "+251 116 180 449" },
-      { name: "ICMC General hospital", phone: "+251 116 678 646" },
-      {
-        name: "Washington Medical Center Addis Ababa",
-        phone: "+251 116 635 969",
-      },
-    ];
-
-  const onContactList = () => {
-    document.getElementsByClassName("landing-page")[0].style.filter = "blur(3px)";
-    document.getElementById('contact-list').style.display = "block"; 
-  };
+    { name: "LandMark Hospital", phone: "+251 115 525 463" },
+    { name: "Addis Cardiac center", phone: "+251 116 634 740" },
+    { name: "Addis Hiwot General Hospital", phone: "+251 116 180 449" },
+    { name: "ICMC General hospital", phone: "+251 116 678 646" },
+    {
+      name: "Washington Medical Center Addis Ababa",
+      phone: "+251 116 635 969",
+    },
+  ];
+  const contactsList = useRef(null);
+  const landingPage = useRef(null);
+  const navigate = useNavigate();
   return (
-    <div style={{ paddingBottom: "20vh", marginBottom: "10vh" }}>
-      <div className="landing-page">
-      <div className="ellipse one"></div>
-      <div className="ellipse two"></div>
-      <div className="hero-section">
-        <div className="hero-texts">
-          <Label text="Welcome!" classes="hero-title">
-            <img
-              className="hero-img"
-              src={assets.stetescope}
-              alt="a stethoscope"
+      <div style={{ paddingBottom: "20vh", marginBottom: "10vh" }} className="landing-page" ref={landingPage}>
+        <div className="ellipse one"></div>
+        <div className="ellipse two"></div>
+        <div className="hero-section">
+          <div className="hero-texts">
+            <Label text="Welcome!" classes="hero-title">
+              <img
+                className="hero-img"
+                src={assets.stetescope}
+                alt="a stethoscope"
+              />
+            </Label>
+            <Label
+              classes="hero-paragraph bold"
+              text="Stay on top of your health anytime, anywhere."
             />
-          </Label>
-          <Label
-            classes="hero-paragraph bold"
-            text="Stay on top of your health anytime, anywhere."
-          />
-          <Label
-            text="Smart health assistance: Accurate, reliable 
+            <Label
+              text="Smart health assistance: Accurate, reliable 
             diagnoses and real-time support whenever you need them."
-            classes="hero-paragraph small"
-          />
-          <Button
-            text="Diagnose"
-            classes="btn primary"
-            style={{ display: "inline-block", fontFamily: "serif" }}
-            sendRequest={onDiagnose}
-          />
-          <Button
-            text="Chat Service"
-            classes="btn secondary"
-            style={{ fontFamily: "serif", display: "inline-block" }}
-            sendRequest={onChatService}
+              classes="hero-paragraph small"
+            />
+            <Button
+              text="Diagnose"
+              classes="btn primary"
+              style={{ display: "inline-block", fontFamily: "serif" }}
+              sendRequest={() => {
+                navigate("/diagnose");
+              }}
+            />
+            <Button
+              text="Chat Service"
+              classes="btn secondary"
+              style={{ fontFamily: "serif", display: "inline-block" }}
+              sendRequest={() => {
+                navigate("/chat");
+              }}
+            />
+          </div>
+          <img
+            className="doctor"
+            src={assets.doctorLanding}
+            alt="doctor waiting for patient with smile"
           />
         </div>
-        <img
-          className="doctor"
-          src={assets.doctorLanding}
-          alt="doctor waiting for patient with smile"
-        />
-      </div>
-      <div className="services-list">
-        <Card
-          classes="service-card"
-          title="Chatbot Service"
-          text="Better care starts here: Chat with us 
+        <div className="services-list">
+          <Card
+            classes="service-card"
+            title="Chatbot Service"
+            text="Better care starts here: Chat with us 
                 for fast and reliable health guidance."
-        />
-
-        <Card
-          classes="service-card"
-          title="Diagnosis Center"
-          text="Smart Health Assistance: Accurate, reliable 
-                diagnoses whenever you need them"
-        />
-        <Card classes="service-card">
-          <Button
-            classes="btn secondary "
-            sendRequest={onContactList}
-            text="Contact List"
           />
-        </Card>
-      </div>
-      </div>
-      <div
-        id="contact-list" 
-        onClick={() => {
-          document.getElementById("contact-list").style.display = 'none';
-          document.getElementsByClassName("landing-page")[0].style.filter = "none";
-        }}
-      >
-        <div className="modal-header" >
+
+          <Card
+            classes="service-card"
+            title="Diagnosis Center"
+            text="Smart Health Assistance: Accurate, reliable 
+                diagnoses whenever you need them"
+          />
+          <Card classes="service-card">
+            <Button
+              classes="btn secondary "
+              sendRequest={()=>{
+                contactsList.current.style.display = "block";
+              }}
+              text="Contact List"
+            />
+          </Card>
+        </div>
+        <ContactsList
+          ref={contactsList}
+          contacts={contacts}
+          onClick={() => {
+            landingPage.current.style.filter = "none";
+            contactsList.current.style.display = "none";
+          }}/>
+    </div>
+  );
+}
+
+export default LandingPage;
+
+function ContactsList({ contacts, ref, onClick, style }) {
+  return (
+    <div id="contact-list" ref={ref} style={style} onClick={onClick}>
+      <div className="modal-header">
         <h2 className="modal-title">Contacts </h2>
         <span className="btn close-modal-btn">X</span>
-        </div>
-        <div className="contacts" >
+      </div>
+      <div className="contacts">
         {contacts.map((contact) => {
           return (
             <div className="contact" key={contact.name}>
@@ -105,10 +117,7 @@ function LandingPage({ onDiagnose, onChatService }) {
             </div>
           );
         })}
-        </div>
       </div>
     </div>
   );
 }
-
-export default LandingPage;
